7655f4612b21410350aec3be7f6ccf1007daba29
Rybalkoyuliia <rybalkoyuliia4@gmail.com>
2024-03-16 10:57:18 -0500

    fixed task 4 and 5 in generic

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -2,14 +2,16 @@
   Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
 */
 
+interface Title {
+  title: string;
+}
+
 class Component<T> {
   constructor(public props: T) {}
 }
 
-class Page extends Component<{ title: any }> {
+class Page extends Component<Title> {
   pageInfo() {
     console.log(this.props.title);
   }
 }
-
-export {};
